657eae9a54fe792b7c4d95f4eb56f05a05b0054f
NCMoseley <ncmoseley@gmail.com>
2018-06-28 18:05:49 -0700

    Add search bar, and bound the callback in the container

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import SeachBar from "../containers/search_bar";
+import SearchBar from "../containers/search_bar";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <SeachBar />
+        <SearchBar />
       </div>
     );
   }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+    // If you are passing around a callback, and that callback has a reference to "this", it needs to be bound.
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    console.log(e.target.value);
+    this.setState({ term: e.target.value });
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button tye="submit" className="btn btn-secondary">
+            Submit
+          </button>
+        </span>
       </form>
     );
   }
 }
+
+export default SearchBar;
